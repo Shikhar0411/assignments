5221da5dce0c13e2bfa0771c0306854b81c86b0c
shivohra <shivohra@paypal.com>
2023-12-04 23:59:45 +0530

    solution to some problems

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,33 @@
 
 function isAnagram(str1, str2) {
 
+    // First check for the lenghts
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    // Add characters of one to a map, and the number of times it appears
+    let characterMap = new Map();
+
+    for (let i = 0; i < str1.length ; i++){
+        let key = str1[i];
+        if (characterMap.has(key)) {
+            characterMap.set(key,characterMap.get(key) + 1);
+        } else {
+            characterMap.set(str1[i],1);
+        }
+    }
+
+    for (let i = 0; i < str2.length ; i++){
+        let key = str2[i];
+        if (!characterMap.has(key)) return false;
+
+        if (characterMap.get(key) === 0) return false;
+
+        characterMap.set(key,characterMap.get(key)-1);
+    }
+
+    return true;
 }
 
 module.exports = isAnagram;
